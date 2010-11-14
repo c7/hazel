ecdb36a59123d75084de2b156473dbb252328083
Peter Hellberg <peter@c7.se>
2010-11-15 00:26:22 +0100

    Initial web interface

--- app/hazel.rb
+++ app/hazel.rb
@@ -2,4 +2,22 @@ class Hazel < Sinatra::Base
   get '/' do
     haml :index, :locals => { :body_class => 'home' }
   end
+  
+  get '/manage' do
+    haml :manage, :locals => { :body_class => 'manage' }
+  end
+  
+  helpers do
+    def base_url
+      @base_url ||= "#{request.env['rack.url_scheme']}://#{request.env['HTTP_HOST']}"
+    end
+    
+    def bookmarklet
+      haml(:bookmarklet, :locals => {
+        :url => "#{base_url}/manage", 
+        :window_name => 'hazel-bookmarklet',
+        :width => 500, :height => 600
+      }).to_s.gsub('\n', '')
+    end
+  end
 end
