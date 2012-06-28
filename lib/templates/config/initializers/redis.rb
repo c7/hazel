0177733a6bd219f0dd5fa018ccce48d54e8f1c3a
Peter Hellberg <peter@c7.se>
2012-06-28 13:57:17 +0200

    Don’t load Redis when running tests.

--- lib/templates/config/initializers/redis.rb
+++ lib/templates/config/initializers/redis.rb
@@ -1,3 +1,5 @@
 # Redis Configuration
-redis_settings = YAML::load_file("config/redis.yml")
-REDIS = Redis.new(redis_settings[ENV['RACK_ENV']])
+unless ENV['RACK_ENV'] == 'test'
+  redis_settings = YAML::load_file("config/redis.yml")
+  REDIS = Redis.new(redis_settings[ENV['RACK_ENV']])
+end
