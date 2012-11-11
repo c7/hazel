33c04eab7ae3ca770e609145d8597c3e6061554a
Peter Hellberg <peter@c7.se>
2012-11-12 00:00:15 +0100

    Removed the instance variables from the controller.

--- lib/templates/app.rb
+++ lib/templates/app.rb
@@ -3,9 +3,6 @@ class <%= @name.camel_case %> < Sinatra::Base
   set :public_folder => "public", :static => true
 
   get "/" do
-    @version     = RUBY_VERSION
-    @environment = ENV['RACK_ENV']
-
     erb :welcome
   end
 end
